5599ff1411a14c6ab06d77e07b2b8ffcbcd71d59
wachirawit wacharak <tauhoo_ice@hotmail.com>
2019-04-20 20:39:18 +0700

    Fix responsive in MusicButtonList

--- src/component/ButtonPage/MusicButtonList.js
+++ src/component/ButtonPage/MusicButtonList.js
@@ -11,6 +11,18 @@ const List = styled.div`
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 80px;
   justify-items: center;
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 820px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 460px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 const list = amount => {
