5051f40c70dd108326b2f82e81caa5186356dac4
wachirawit wacharak <tauhoo_ice@hotmail.com>
2019-04-20 18:41:47 +0700

    Set up route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-import React, { Component } from 'react'
-import { Icon } from 'antd'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Icon type="warning" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Ice
-          </a>
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default () => <h1>iceee</h1>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import 'antd/dist/antd.css'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const pages = [{ url: '/', page: App }]
+
+const root = (
+  <Router>
+    {pages.map(({ url, page }) => (
+      <Route exact>{page}</Route>
+    ))}
+  </Router>
+)
+
+ReactDOM.render(root, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
